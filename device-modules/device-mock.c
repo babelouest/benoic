5114b0dcc98053bd3ddd8f35991df9e97d84f656
Nicolas Mora <mail@babelouest.org>
2016-08-11 14:36:50 -0400

    Add documentation and clean some code

--- device-modules/device-mock.c
+++ device-modules/device-mock.c
@@ -21,11 +21,11 @@
 #include <yder.h>
 #include <orcania.h>
 
-#define RESULT_ERROR     0
-#define RESULT_OK        1
-#define RESULT_NOT_FOUND 2
-#define RESULT_TIMEOUT   3
-#define RESULT_PARAM     4
+#define WEBSERVICE_RESULT_ERROR     0
+#define WEBSERVICE_RESULT_OK        1
+#define WEBSERVICE_RESULT_NOT_FOUND 2
+#define WEBSERVICE_RESULT_TIMEOUT   3
+#define WEBSERVICE_RESULT_PARAM     4
 
 #define ELEMENT_TYPE_NONE   0
 #define ELEMENT_TYPE_SENSOR 1
@@ -67,7 +67,7 @@ json_t * b_device_type_init () {
   json_array_append_new(options, json_pack("{ssssssso}", "name", "do_not_check_certificate", "type", "boolean", "description", "check the certificate of the device if needed", "optional", json_true()));
   json_array_append_new(options, json_pack("{ssssssso}", "name", "device_specified", "type", "string", "description", "specified by the device when connected for the first time, then must be sent back at every other connection", "optional", json_true()));
   return json_pack("{sissssssso}", 
-                    "result", RESULT_OK,
+                    "result", WEBSERVICE_RESULT_OK,
                     "uid", "00-00-00", 
                     "name", "Another Mock Device", 
                     "description", "This is another mock device, for development and debug purposes", 
@@ -95,10 +95,10 @@ json_t * b_device_connect (json_t * device, void ** device_ptr) {
   
   if (nstrstr(json_string_value(json_object_get(json_object_get(device, "options"), "device_specified")), "batman") == NULL) {
     param = msprintf("%s says I'm batman with the alert_url %s", json_string_value(json_object_get(device, "name")), json_string_value(json_object_get(json_object_get(device, "options"), "alert_url")));
-    j_param = json_pack("{sis{ss}}", "result", RESULT_OK, "options", "device_specified", param);
+    j_param = json_pack("{sis{ss}}", "result", WEBSERVICE_RESULT_OK, "options", "device_specified", param);
     free(param);
   } else {
-    j_param = json_pack("{si}", "result", RESULT_OK);
+    j_param = json_pack("{si}", "result", WEBSERVICE_RESULT_OK);
   }
   return j_param;
 }
@@ -111,14 +111,14 @@ json_t * b_device_disconnect (json_t * device, void * device_ptr) {
     // Free device_ptr
     json_decref((json_t *)device_ptr);
   }
-  return json_pack("{si}", "result", RESULT_OK);
+  return json_pack("{si}", "result", WEBSERVICE_RESULT_OK);
 }
 
 /**
  * Ping the device type
  */
 json_t * b_device_ping (json_t * device, void * device_ptr) {
-  return json_pack("{si}", "result", RESULT_OK);
+  return json_pack("{si}", "result", WEBSERVICE_RESULT_OK);
 }
 
 /**
@@ -138,7 +138,7 @@ json_t * b_device_overview (json_t * device, void * device_ptr) {
     
   json_t * result = json_pack("{sis{s{sssf}s{sosf}}s{sIsI}s{sIsI}s{soso}}",
                              "result", 
-                             RESULT_OK,
+                             WEBSERVICE_RESULT_OK,
                              "sensors", 
                                 "se1", 
                                   "unit", "C",
@@ -168,9 +168,9 @@ json_t * b_device_overview (json_t * device, void * device_ptr) {
 json_t * b_device_get_sensor (json_t * device, const char * sensor_name, void * device_ptr) {
   y_log_message(Y_LOG_LEVEL_INFO, "device-mock - Running command sensor for sensor %s on device %s", sensor_name, json_string_value(json_object_get(device, "name")));
   if (0 == nstrcmp(sensor_name, "se1") || 0 == nstrcmp(sensor_name, "se2")) {
-    return json_pack("{sisf}", "result", RESULT_OK, "value", get_sensor_value(sensor_name));
+    return json_pack("{sisf}", "result", WEBSERVICE_RESULT_OK, "value", get_sensor_value(sensor_name));
   } else {
-    return json_pack("{si}", "result", RESULT_NOT_FOUND);
+    return json_pack("{si}", "result", WEBSERVICE_RESULT_NOT_FOUND);
   }
 }
 
@@ -180,11 +180,11 @@ json_t * b_device_get_sensor (json_t * device, const char * sensor_name, void * 
 json_t * b_device_get_switch (json_t * device, const char * switch_name, void * device_ptr) {
   y_log_message(Y_LOG_LEVEL_INFO, "device-mock - Running command get_switch for switch %s on device %s", switch_name, json_string_value(json_object_get(device, "name")));
   if (0 == nstrcmp(switch_name, "sw1")) {
-    return json_pack("{sisi}", "result", RESULT_OK, "value", json_integer_value(json_object_get(json_object_get((json_t *)device_ptr, "switches"), "sw1")));
+    return json_pack("{sisi}", "result", WEBSERVICE_RESULT_OK, "value", json_integer_value(json_object_get(json_object_get((json_t *)device_ptr, "switches"), "sw1")));
   } else if (0 == nstrcmp(switch_name, "sw2")) {
-    return json_pack("{sisi}", "result", RESULT_OK, "value", json_integer_value(json_object_get(json_object_get((json_t *)device_ptr, "switches"), "sw2")));
+    return json_pack("{sisi}", "result", WEBSERVICE_RESULT_OK, "value", json_integer_value(json_object_get(json_object_get((json_t *)device_ptr, "switches"), "sw2")));
   } else {
-    return json_pack("{si}", "result", RESULT_NOT_FOUND);
+    return json_pack("{si}", "result", WEBSERVICE_RESULT_NOT_FOUND);
   }
 }
 
@@ -195,9 +195,9 @@ json_t * b_device_set_switch (json_t * device, const char * switch_name, const i
   y_log_message(Y_LOG_LEVEL_INFO, "device-mock - Running command set_switch for switch %s on device %s with the value %d", switch_name, json_string_value(json_object_get(device, "name")), command);
   if (0 == nstrcmp(switch_name, "sw1") || 0 == nstrcmp(switch_name, "sw2")) {
     json_object_set_new(json_object_get((json_t *)device_ptr, "switches"), switch_name, json_integer(command));
-    return json_pack("{si}", "result", RESULT_OK);
+    return json_pack("{si}", "result", WEBSERVICE_RESULT_OK);
   } else {
-    return json_pack("{si}", "result", RESULT_NOT_FOUND);
+    return json_pack("{si}", "result", WEBSERVICE_RESULT_NOT_FOUND);
   }
 }
 
@@ -207,9 +207,9 @@ json_t * b_device_set_switch (json_t * device, const char * switch_name, const i
 json_t * b_device_get_dimmer (json_t * device, const char * dimmer_name, void * device_ptr) {
   y_log_message(Y_LOG_LEVEL_INFO, "device-mock - Running command get_dimmer for dimmer %s on device %s", dimmer_name, json_string_value(json_object_get(device, "name")));
   if (0 == nstrcmp(dimmer_name, "di1") || 0 == nstrcmp(dimmer_name, "di2")) {
-    return json_pack("{sisi}", "result", RESULT_OK, "value", json_integer_value(json_object_get(json_object_get((json_t *)device_ptr, "dimmers"), dimmer_name)));
+    return json_pack("{sisi}", "result", WEBSERVICE_RESULT_OK, "value", json_integer_value(json_object_get(json_object_get((json_t *)device_ptr, "dimmers"), dimmer_name)));
   } else {
-    return json_pack("{si}", "result", RESULT_NOT_FOUND);
+    return json_pack("{si}", "result", WEBSERVICE_RESULT_NOT_FOUND);
   }
 }
 
@@ -220,9 +220,9 @@ json_t * b_device_set_dimmer (json_t * device, const char * dimmer_name, const i
   y_log_message(Y_LOG_LEVEL_INFO, "device-mock - Running command set_dimmer for dimmer %s on device %s with the value %d", dimmer_name, json_string_value(json_object_get(device, "name")), command);
   if (0 == nstrcmp(dimmer_name, "di1") || 0 == nstrcmp(dimmer_name, "di2")) {
     json_object_set_new(json_object_get((json_t *)device_ptr, "dimmers"), dimmer_name, json_integer(command));
-    return json_pack("{si}", "result", RESULT_OK);
+    return json_pack("{si}", "result", WEBSERVICE_RESULT_OK);
   } else {
-    return json_pack("{si}", "result", RESULT_NOT_FOUND);
+    return json_pack("{si}", "result", WEBSERVICE_RESULT_NOT_FOUND);
   }
 }
 
@@ -233,14 +233,14 @@ json_t * b_device_get_heater (json_t * device, const char * heater_name, void * 
   y_log_message(Y_LOG_LEVEL_INFO, "device-mock - Running command get_heater for heater %s on device %s", heater_name, json_string_value(json_object_get(device, "name")));
   if (0 == nstrcmp(heater_name, "he1")) {
     json_t * heater = json_copy(json_object_get(json_object_get((json_t *)device_ptr, "heaters"), heater_name));
-    json_object_set_new(heater, "result", json_integer(RESULT_OK));
+    json_object_set_new(heater, "result", json_integer(WEBSERVICE_RESULT_OK));
     return heater;
   } else if (0 == nstrcmp(heater_name, "he2")) {
     json_t * heater = json_copy(json_object_get(json_object_get((json_t *)device_ptr, "heaters"), heater_name));
-    json_object_set_new(heater, "result", json_integer(RESULT_OK));
+    json_object_set_new(heater, "result", json_integer(WEBSERVICE_RESULT_OK));
     return heater;
   } else {
-    return json_pack("{si}", "result", RESULT_NOT_FOUND);
+    return json_pack("{si}", "result", WEBSERVICE_RESULT_NOT_FOUND);
   }
 }
 
@@ -258,12 +258,12 @@ json_t * b_device_set_heater (json_t * device, const char * heater_name, const c
     } else if (mode != NULL && 0 == nstrcmp(mode, BENOIC_ELEMENT_HEATER_MODE_OFF)) {
       json_object_set_new(heater, "mode", json_string(BENOIC_ELEMENT_HEATER_MODE_OFF));
     } else if (mode != NULL) {
-      return json_pack("{si}", "result", RESULT_PARAM);
+      return json_pack("{si}", "result", WEBSERVICE_RESULT_PARAM);
     }
     json_object_set_new(heater, "command", json_real(command));
-    return json_pack("{si}", "result", RESULT_OK);
+    return json_pack("{si}", "result", WEBSERVICE_RESULT_OK);
   } else {
-    return json_pack("{si}", "result", RESULT_NOT_FOUND);
+    return json_pack("{si}", "result", WEBSERVICE_RESULT_NOT_FOUND);
   }
 }
 
